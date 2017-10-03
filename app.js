3455cdee57fd5115a7bbf8113d22249fce802ee6
Raymond Kim <ray.kim09@gmail.com>
2017-10-03 17:09:49 -0400

    completed assignment, without math.random

---
+++ app.js
@@ -0,0 +1,5 @@
+var mathlib = require('./mathlib')();  
+console.log(mathlib)
+console.log(mathlib.add(4,5))
+console.log(mathlib.multiply(5,1999))
+console.log(mathlib.square(22))
---
+++ mathlib.js
@@ -0,0 +1,19 @@
+module.exports = function (){
+    return {
+      add: function(num1, num2) { 
+           // add code here 
+           let sumtotal = num1 + num2;
+           return sumtotal
+      },
+      multiply: function(num1, num2) {
+           // add code here 
+           let multtotal = num1 * num2
+           return multtotal
+      },
+      square: function(num) {
+           // add code here 
+           let sqrtotal = num * num
+           return sqrtotal
+      },
+    }
+  };
